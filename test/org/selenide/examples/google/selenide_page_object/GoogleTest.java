d6511b14273235f43f2e5a93b2ef91217d6e4833
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-12-26 21:49:56 +0200

    better test

--- test/org/selenide/examples/google/selenide_page_object/GoogleTest.java
+++ test/org/selenide/examples/google/selenide_page_object/GoogleTest.java
@@ -2,6 +2,7 @@ package org.selenide.examples.google.selenide_page_object;
 
 import org.junit.Test;
 
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -10,6 +11,7 @@ public class GoogleTest {
   public void userCanSearch() {
     GooglePage page = open("http://google.com/ncr", GooglePage.class);
     SearchResultsPage results = page.searchFor("selenide");
-    results.getResults().get(0).shouldHave(text("Selenide: concise UI tests in Java"));
+    results.getResults().shouldHave(size(10));
+    results.getResult(0).shouldHave(text("Selenide: concise UI tests in Java"));
   }
 }
--- test/org/selenide/examples/google/selenide_page_object/SearchResultsPage.java
+++ test/org/selenide/examples/google/selenide_page_object/SearchResultsPage.java
@@ -1,11 +1,16 @@
 package org.selenide.examples.google.selenide_page_object;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class SearchResultsPage {
   public ElementsCollection getResults() {
     return $$("#ires .g");
+  }
+  public SelenideElement getResult(int index) {
+    return $("#ires .g", index);
   }
 }

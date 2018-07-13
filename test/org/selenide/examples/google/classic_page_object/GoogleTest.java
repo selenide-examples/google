3feb5bbceb70358a05c52680a408223e64fff1fe
Andrei Solntsev <andrei.solntsev@gmail.com>
2018-07-14 00:45:19 +0300

    add maven/gradle tasks for running tests in all possible browsers

--- test/org/selenide/examples/google/classic_page_object/GoogleTest.java
+++ test/org/selenide/examples/google/classic_page_object/GoogleTest.java
@@ -1,22 +1,48 @@
 package org.selenide.examples.google.classic_page_object;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.PageFactory;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertThat;
 
 public class GoogleTest {
   WebDriver driver;
 
   @Before
   public void setUp() {
-    driver = "chrome".equals(System.getProperty("selenide.browser")) ? 
-        new ChromeDriver() : new FirefoxDriver();
+    String currentBrowser = System.getProperty("selenide.browser", "firefox");
+    if ("chrome".equals(currentBrowser)) {
+      WebDriverManager.chromedriver().setup();
+      driver = new ChromeDriver();
+    } else if ("firefox".equals(currentBrowser)) {
+      WebDriverManager.firefoxdriver().setup();
+      driver = new FirefoxDriver();
+    } else if ("phantomjs".equals(currentBrowser)) {
+      WebDriverManager.phantomjs().setup();
+      driver = new PhantomJSDriver();
+    } else if ("safari".equals(currentBrowser)) {
+      driver = new SafariDriver();
+    } else if ("edge".equals(currentBrowser)) {
+      WebDriverManager.edgedriver().setup();
+      driver = new EdgeDriver();
+    } else if ("ie".equals(currentBrowser)) {
+      WebDriverManager.iedriver().setup();
+      driver = new InternetExplorerDriver();
+    } else if ("htmlunit".equals(currentBrowser)) {
+      driver = new HtmlUnitDriver();
+    }
   }
 
   @After
@@ -28,9 +54,9 @@ public class GoogleTest {
 
   @Test
   public void userCanSearch() {
-    driver.get("http://www.google.com/");
+    driver.get("https://www.google.com/ncr");
     GooglePage page = PageFactory.initElements(driver, GooglePage.class);
     SearchResultsPage results = page.searchFor("Selenide");
-    assertTrue(results.getResults().get(0).getText().startsWith("Selenide: concise UI tests in Java"));
+    assertThat(results.getResults().get(0).getText(), startsWith("Selenide: concise UI tests in Java"));
   }
 }

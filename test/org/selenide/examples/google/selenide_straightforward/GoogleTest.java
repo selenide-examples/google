6c5e688778ed30efa91b2db6f04057deadaad431
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-04-12 22:18:13 +0300

    add TestReport to GoogleTest

--- test/org/selenide/examples/google/selenide_straightforward/GoogleTest.java
+++ test/org/selenide/examples/google/selenide_straightforward/GoogleTest.java
@@ -1,6 +1,9 @@
 package org.selenide.examples.google.selenide_straightforward;
 
+import com.codeborne.selenide.junit.TextReport;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -9,6 +12,9 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleTest {
+  @Rule
+  public TestRule report = new TextReport().onFailedTest(true).onSucceededTest(true);
+
   @Test
   public void search_selenide_in_google() {
     open("https://google.com/ncr");

221f9070fd0761461e9aa30a78b8911168a21120
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-03-12 23:32:36 +0200

    bump Selenide from 5.25.0 to 6.12.2

--- test/org/selenide/examples/google/classic_page_object/GooglePage.java
+++ test/org/selenide/examples/google/classic_page_object/GooglePage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class GooglePage {
@@ -19,7 +21,7 @@ public class GooglePage {
   public SearchResultsPage searchFor(String text) {
     q.sendKeys(text);
     q.submit();
-    new WebDriverWait(webdriver, 8).until(visibilityOfElementLocated(By.cssSelector(".results .result")));
+    new WebDriverWait(webdriver, Duration.ofSeconds(8)).until(visibilityOfElementLocated(By.cssSelector(".results .result")));
     return PageFactory.initElements(webdriver, SearchResultsPage.class);
   }
 }

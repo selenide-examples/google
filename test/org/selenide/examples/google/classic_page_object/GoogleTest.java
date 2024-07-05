60367e54411dca229224be7a397a58ccc148cfa2
Aliaksandr Rasolka <rosolko@gmail.com>
2024-07-05 10:27:34 +0300

    Update dependencies. Add maven wrapper. Update readme. Code cleanup (#164)

--- test/org/selenide/examples/google/classic_page_object/GoogleTest.java
+++ test/org/selenide/examples/google/classic_page_object/GoogleTest.java
@@ -1,8 +1,8 @@
 package org.selenide.examples.google.classic_page_object;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -11,13 +11,12 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.PageFactory;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GoogleTest {
   private WebDriver driver;
 
-  @Before
+  @BeforeEach
   public void setUp() {
     String currentBrowser = System.getProperty("selenide.browser", "firefox");
     if ("chrome".equals(currentBrowser)) {
@@ -33,7 +32,7 @@ public class GoogleTest {
     }
   }
 
-  @After
+  @AfterEach
   public void tearDown() {
     if (driver != null) {
       driver.quit();
@@ -45,6 +44,8 @@ public class GoogleTest {
     driver.get("https://www.duckduckgo.com");
     GooglePage page = PageFactory.initElements(driver, GooglePage.class);
     SearchResultsPage results = page.searchFor("Selenide java");
-    assertThat(results.getResults().get(0).getText(), containsString("Selenide: concise UI tests in Java"));
+    assertThat(results.getResults().get(0).getText())
+            .isNotNull()
+            .contains("\"Selenide: concise UI tests in Java\"");
   }
 }
--- test/org/selenide/examples/google/selenide_page_object/GooglePage.java
+++ test/org/selenide/examples/google/selenide_page_object/GooglePage.java
@@ -3,7 +3,6 @@ package org.selenide.examples.google.selenide_page_object;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.page;
 
 public class GooglePage {
   public void searchFor(String text) {
